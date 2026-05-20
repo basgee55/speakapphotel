50d9fe500974c087b8dd0dfc784836531e1c07d5
basgee55 <basgeevers@gmail.com>
2026-05-20 22:27:03 +0200

    Auto-detect first available voice from ElevenLabs account

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lumiere-v11';
+const CACHE = 'lumiere-v12';
 const STATIC = ['./manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 const BYPASS = ['api.anthropic.com', 'api.elevenlabs.io', 'fonts.googleapis.com', 'fonts.gstatic.com', 'workers.dev'];
 
