49f952e11e414c57837b34fe48e39ce3e8d3f8c0
basgee55 <basgeevers@gmail.com>
2026-05-20 23:02:50 +0200

    Add Web Speech API TTS: receptionist speaks AI replies in Dutch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lumiere-v13';
+const CACHE = 'lumiere-v14';
 const STATIC = ['./manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 const BYPASS = ['api.anthropic.com', 'api.elevenlabs.io', 'fonts.googleapis.com', 'fonts.gstatic.com', 'workers.dev'];
 
