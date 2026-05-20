8d3376761e276ca5b70be5429c39a4fdda26c29f
basgee55 <basgeevers@gmail.com>
2026-05-20 23:39:57 +0200

    TTS: prefer Google Nederlands, fallback Microsoft Frank, remove debug button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lumiere-v18';
+const CACHE = 'lumiere-v19';
 const STATIC = ['./manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 const BYPASS = ['api.anthropic.com', 'api.elevenlabs.io', 'fonts.googleapis.com', 'fonts.gstatic.com', 'workers.dev'];
 
